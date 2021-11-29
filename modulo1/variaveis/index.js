27d4e4236dfedd61f33cdb914c6d1346e7f6945b
Pedro Acciainoli <pedro.acciainoli@gmail.com>
2021-11-29 13:52:48 -0300

    Entrega da aula variaveis + desafio

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,48 @@
+/*  1. Analise o programa abaixo e diga o que será impresso no console, **SEM EXECUTAR o programa.**
+    
+```jsx
+let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)
+```
+
+let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b) 
+
+Saida = 10 10,5
+*/
+
+
+/* 2.Analise o programa abaixo e diga o que será impresso no console, SEM EXECUTAR o programa.
+
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)
+
+Saida = 10 10 10 
+ */
+
+/* 3.Analise o programa abaixo, entenda o que ele faz e sugira melhores nomes para as variáveis. Lembre-se que devemos escolher nomes significativos, usar o padrão camelCase. Alem disso, os nomes não podem começar com números ou caracteres especiais.
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+
+let p = let HorasTrabalhadas
+let t = let ValorDia
+
+*/
+
+
