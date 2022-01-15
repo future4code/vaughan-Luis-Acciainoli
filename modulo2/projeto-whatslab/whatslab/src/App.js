0f76b7ccf458bde3f18547c4d7a5efe393cf8c78
Pedro Acciainoli <pedro.acciainoli@gmail.com>
2022-01-14 22:05:06 -0300

    sidebar do whatsapp incompleto

--- modulo2/projeto-whatslab/whatslab/src/App.js
+++ modulo2/projeto-whatslab/whatslab/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import ChatListaItem from './components/ChatListaItem';
 
-function App() {
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+
+export default () => {
+
+
+  const [chatLista, setChatLista] = useState([{},{},{}]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='appJanela'>
+      <div className='sidebar'>
+        
+        <header>
+          <img className='avatarTopo'src='https://i.pravatar.cc/300' alt='imagem avatar'></img>
+          <h1>Whatslab</h1>
+          <div className='botoes-topo'>
+
+            <div className='btn-topo'>
+              <DonutLargeIcon style={{color: '#F27405'}} />
+            </div>
+            <div className='btn-topo'>
+              <ChatIcon style={{color: '#F27405'}} />
+            </div>
+            <div className='btn-topo'>
+              <MoreVertIcon style={{color: '#F27405'}} />
+            </div>
+
+          </div>
+        </header>
+
+
+        <div className='buscar'>
+          <div className='buscar-input'>
+            <SearchIcon style={{color: '#F27405'}} fontSize='small' />
+            <input type="search" placeholder="Pesquisar ou começar uma nova conversa"></input>
+          </div>
+        </div>
+
+        <div className='chatLista'>  
+          {/* Chamar os componentes de chat */}
+          {chatLista.map((item, key) =>
+            <ChatListaItem
+             key={key}
+            />
+          ))}
+        </div>
+      </div>
+      <div className='conteudo'>
+        conteudo
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ modulo2/projeto-whatslab/whatslab/src/components/ChatListaItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './ChatListaItem.css';
+
+export default () => {
+    return (
+        <div>teste</div>
+    )
+}
