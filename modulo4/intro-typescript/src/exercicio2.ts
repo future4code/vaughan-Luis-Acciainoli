8599d55a41bf6c509da17c7fe85aef3561054209
Pedro Acciainoli <pedro.acciainoli@gmail.com>
2022-03-23 22:22:50 -0300

    entrega

---
+++ modulo4/intro-typescript/src/exercicio2.ts
@@ -0,0 +1,8 @@
+function imprimeTresCoresFavoritas(cor1: string, cor2: string, cor3: string)  {
+    const cores = []
+  
+    cores.push(cor1,cor2,cor3) 
+  
+    return cores
+  }
+  imprimeTresCoresFavoritas("verde", "amarelo", "roxo");
