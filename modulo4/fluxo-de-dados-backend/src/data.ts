6532d7ebc3b8e1ba62a26ef2f31afbed4da4649d
pedro.acciainoli <pedro.acciainoli@gmail.com>
2022-04-03 21:45:40 -0300

    exercicio fluxo de dados :tada:

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,28 @@
+export type Produto = {
+    id: string,
+    name: string,
+    price: number
+}
+
+export const produtos: Produto[] = [
+    {
+        id: "1",
+        name: "caneta",
+        price: 2
+    },
+    {
+        id: "2",
+        name: "lapisera",
+        price: 5
+    },
+    {
+        id: "3",
+        name: "lápis",
+        price: 1
+    },
+    {
+        id: "4",
+        name: "caderno",
+        price: 10
+    }
+] 
